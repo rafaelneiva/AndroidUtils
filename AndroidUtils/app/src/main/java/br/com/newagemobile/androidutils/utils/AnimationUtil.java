79ce5b00de830305a18b326f3afb297efd524a89
rafaelneiva <faelviolero@gmail.com>
2015-03-23 17:03:39 -0300

    Add AnimationUtil

---
+++ AndroidUtils/app/src/main/java/br/com/newagemobile/androidutils/utils/AnimationUtil.java
@@ -0,0 +1,66 @@
+package br.com.newagemobile.androidutils.utils;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+/**
+ * Created by rafaelneiva on 3/23/15.
+ */
+public class AnimationUtil {
+
+    // expand a View with animation
+    public static void expand(final View v) {
+        v.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        final int targetHeight = v.getMeasuredHeight();
+
+        v.getLayoutParams().height = 0;
+        v.setVisibility(View.VISIBLE);
+        Animation a = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                v.getLayoutParams().height = interpolatedTime == 1
+                        ? ViewGroup.LayoutParams.WRAP_CONTENT
+                        : (int) (targetHeight * interpolatedTime);
+                v.requestLayout();
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        // 1dp/ms + 100
+        a.setDuration((int) (targetHeight / v.getContext().getResources().getDisplayMetrics().density) + 100);
+        v.startAnimation(a);
+    }
+
+    // collapse a View with animation
+    public static void collapse(final View v) {
+        final int initialHeight = v.getMeasuredHeight();
+
+        Animation a = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if (interpolatedTime == 1) {
+                    v.setVisibility(View.GONE);
+                } else {
+                    v.getLayoutParams().height = initialHeight - (int) (initialHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        // 1dp/ms + 100
+        a.setDuration((int) (initialHeight / v.getContext().getResources().getDisplayMetrics().density) + 100);
+        v.startAnimation(a);
+    }
+
+}
--- AndroidUtils/app/src/main/java/br/com/newagemobile/androidutils/utils/Util.java
+++ AndroidUtils/app/src/main/java/br/com/newagemobile/androidutils/utils/Util.java
@@ -1,5 +1,9 @@
 package br.com.newagemobile.androidutils.utils;
 
+import android.content.Context;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.SimpleDateFormat;
@@ -12,6 +16,28 @@ import java.util.Locale;
  * Created by rafaelneiva on 3/23/15.
  */
 public class Util {
+
+    // ********** JSON Utils ********** //
+
+    // return a String JSON from a .json file on assets folder
+    public static String loadJSONFromAsset(Context ctx, String jsonFile) {
+        String json;
+        try {
+            InputStream is = ctx.getAssets().open(jsonFile);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+//            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        return json;
+    }
+
+    // ********** JSON Utils ********** //
 
 
     // ********** Format Utils ********** //
@@ -29,7 +55,7 @@ public class Util {
 
     // return a String formatted with 'currency'
     public static String formatValue(Double value, String currency) {
-        if(value == null)
+        if (value == null)
             return "";
         DecimalFormat format = new DecimalFormat("##,###,###,##0.00", new DecimalFormatSymbols(new Locale("pt", "BR")));
         format.setMinimumFractionDigits(2);
