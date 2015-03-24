7064cf0e84210b48c7ec1146a1b264046a0a8425
rafaelneiva <faelviolero@gmail.com>
2015-03-24 15:53:49 -0300

    Add Mask.java

---
+++ AndroidUtils/app/src/main/java/br/com/newagemobile/androidutils/utils/Mask.java
@@ -0,0 +1,59 @@
+package br.com.newagemobile.androidutils.utils;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+/**
+ * Created by rafaelneiva on 3/24/15.
+ */
+public class Mask {
+
+    public static String unmask(String s) {
+        return s.replaceAll("[.]", "").replaceAll("[-]", "")
+                .replaceAll("[/]", "").replaceAll("[(]", "")
+                .replaceAll("[)]", "");
+    }
+
+    public static TextWatcher insert(final String mask, final EditText ediTxt) {
+        return new TextWatcher() {
+            boolean isUpdating;
+            String old = "";
+
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
+
+            public void afterTextChanged(Editable s) {
+                String str = Mask.unmask(s.toString());
+                String mascara = "";
+                if (isUpdating) {
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+                int i = 0;
+                for (char m : mask.toCharArray()) {
+                    if (m != '#' && str.length() > old.length()) {
+                        mascara += m;
+                        continue;
+                    }
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                ediTxt.setText(mascara);
+                ediTxt.setSelection(mascara.length());
+            }
+        };
+    }
+}
