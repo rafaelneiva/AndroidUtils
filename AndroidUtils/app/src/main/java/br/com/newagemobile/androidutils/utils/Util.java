709b7dfc5af40ee6132d95911c6a6ce2cd99da6c
rafaelneiva <faelviolero@gmail.com>
2015-05-21 16:49:19 -0300

    Add methods

--- AndroidUtils/app/src/main/java/br/com/newagemobile/androidutils/utils/Util.java
+++ AndroidUtils/app/src/main/java/br/com/newagemobile/androidutils/utils/Util.java
@@ -28,6 +28,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.InputMismatchException;
 import java.util.Locale;
 
 /**
@@ -134,6 +135,56 @@ public class Util {
 
 
     // ********** Misc Utils ********** //
+
+
+    /**
+     * @param CPF CPF number
+     * @return True if CPF is valid
+     */
+    public static boolean validateCPF(String CPF) {
+        if (CPF.equals("00000000000") || CPF.equals("11111111111") || CPF.equals("22222222222") || CPF.equals("33333333333") || CPF.equals("44444444444") || CPF.equals("55555555555") || CPF.equals("66666666666") || CPF.equals("77777777777") || CPF.equals("88888888888") || CPF.equals("99999999999") || (CPF.length() != 11))
+            return (false);
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+        try {
+            sm = 0;
+            peso = 10;
+            for (i = 0; i < 9; i++) {
+                num = (int) (CPF.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11)) dig10 = '0';
+            else
+                dig10 = (char) (r + 48);
+            sm = 0;
+            peso = 11;
+            for (i = 0; i < 10; i++) {
+                num = (int) (CPF.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11)) dig11 = '0';
+            else
+                dig11 = (char) (r + 48);
+            return (dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10));
+        } catch (InputMismatchException erro) {
+            return (false);
+        }
+    }
+
+    /**
+     * @param email Email
+     * @return True if is a valid type of email
+     */
+    public static boolean validateEmail(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
 
     /**
      * @param ctx            Context of application
